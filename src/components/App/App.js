c6d25e01cd56732d21cd733302e38fd977fb7267
ekaterina balabanidis <mythos.kath.nidis@gmail.com>
2023-09-12 19:34:45 +0300

    feat: создать компонент Promo

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
 import './App.css';
+import { Promo } from '../Promo/Promo';
 
 function App() {
   return (
     <div className="page">
+      {/* <Header /> */}
+      <Promo />
+      {/* <AboutProject />
+      <Techs />
+      <AboutMe />
+      <Portfolio />
+      <Footer /> */}
     </div>
   );
 }
---
+++ src/components/Promo/Promo.js
@@ -0,0 +1,27 @@
+import React from "react";
+import './Promo.css';
+// import { NavTab } from '../NavTab/NavTab';
+import globe from '../../images/promo-logo.svg';
+
+function Promo() {
+  return (
+    <section className="promo">
+      <div className="promo__content">
+        <h1 className="promo__title">Учебный проект студента факультета Веб-разработки</h1>
+        <img src={globe} className="promo__image" alt="Изображение глобуса" />
+        <p className="promo__text">
+        Листайте ниже, чтобы узнать больше про этот проект и&nbsp;его создателя.
+        </p>
+        <button
+          className="promo-button"
+          type="button"
+          /* onClick={} */
+          aria-label="узнать больше">
+          Узнать больше
+        </button>
+      </div>
+    </section>
+  );
+}
+
+export { Promo };

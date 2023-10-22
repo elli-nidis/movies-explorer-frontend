23cf081a067b120ef5e1bcd24824a641c0948af7
ekaterina balabanidis <mythos.kath.nidis@gmail.com>
2023-10-22 12:25:57 +0300

    feat: убрать required в Prifile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -42,12 +42,12 @@ function Profile() {
             <>
               <label htmlFor="input-name" className="form__label form__label_two-side-style">
                 <span className="form__label-text">Имя</span>
-                <input className="form__input form__input_right-side" id="input-name" type="text" name="name" value={formValue.name} onChange={handleChange} required />
+                <input className="form__input form__input_right-side" id="input-name" type="text" name="name" value={formValue.name} onChange={handleChange} placeholder="Введите имя" minLength="1" maxLength="30" />
                 <span className="form__error">{err.message || ""}</span>
               </label>
               <label htmlFor="input-email" className="form__label form__label_two-side-style">
                 <span className="form__label-text">E-mail</span>
-                <input className="form__input form__input_right-side" id="input-email" type="email" name="email" value={formValue.email} onChange={handleChange} required />
+                <input className="form__input form__input_right-side" id="input-email" type="email" name="email" value={formValue.email} onChange={handleChange} maxLength="30" placeholder="Введите e-mail" />
                 <span className="form__error">{err.message || ""}</span>
               </label>
             </>

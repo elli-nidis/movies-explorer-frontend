0e447d08936c5cc910ca4ff77c919e01c7db793d
ekaterina balabanidis <mythos.kath.nidis@gmail.com>
2023-11-07 22:41:29 +0300

    fix: изменить адрес сервера

--- src/utils/AddressApiConfig.js
+++ src/utils/AddressApiConfig.js
@@ -1,5 +1,5 @@
 export const BASE_URL_MOVIES = "https://api.nomoreparties.co/beatfilm-movies";
 
-export const BASE_URL = "http://localhost:3000";
+// export const BASE_URL = "http://localhost:3000";
 
-// export const BASE_URL = 'https://api.mov.nomoredomainsicu.ru';
+export const BASE_URL = 'https://api.mov.nomoredomainsicu.ru';
